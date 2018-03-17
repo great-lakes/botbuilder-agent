155511dc3b5be6715baa02d19cf7d66a4e85668d
Kevin Leung <leung.kevin23@gmail.com>
2018-03-17 13:59:21 -0500

    initial agent src done

--- src/index.ts
+++ src/index.ts
@@ -2,48 +2,27 @@
  * @module botbuilder-agent
  */
 
-import fetch from 'node-fetch'
 import { DirectLine } from 'botframework-directlinejs'
 
-/**
- * Interface Options:
- * OPTION 1: User pases in function which returns a promise (resolves the message string(s))
- * OPTION 1: Pro - user defines how to handle resonse object and parse (probably best this way)
- * 
- * OPTION 2: User passes in payload and endpoint and we construct a fetch call
- * OPTION 2: Pro - allows us to control the call library
- */
+export interface ResponseObject {
+  message: string
+}
 
-/**
- * Things to decide:
- * - How do we handle this in wolf intake?
- * - Generally, how do users know this directline message is from `agent` to handle accordingly?
- */
+// USAGE:
+// import { makeAgent } from 'botbuilder-agent
+// const agent = makeAgent('directlineSecretString')
+// agent(callPromise)
 
-var directLine = new DirectLine({
-  secret: '/* put your Direct Line secret here */',
-  token: '/* or put your Direct Line token here (supply secret OR token, not both) */'
-})
+export function makeAgent (secret: string) {
+  const directLine = new DirectLine({secret})
 
-// TODO: delete temp (result should be captured in promise chain)
-const responseString = 'response string'
-
-export function makeCall (context: any, /* call: () => {} */) {
-
-  // TODO: delete mock function: user passes in function to call
-  const call = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('response string')
-      }, 3000)
+  return (call: () => Promise<ResponseObject>) => {
+    call().then((responseObj) => {
+      directLine.postActivity({
+        from: { id: 'agent', name: 'wolf' },
+        type: 'message',
+        text: responseObj.message
+      })
     })
   }
-
-  call().then((/* resoponseString */) => {
-    directLine.postActivity({
-      from: { id: 'myUserId', name: 'myUserName' }, // required (from.name is optional)
-      type: 'message',
-      text: responseString
-    })
-  })
 }
